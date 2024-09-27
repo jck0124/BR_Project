cf2abde412fd89dc80d57ef710cd551c13051924
윤수빈 <@192.168.105.98>
2024-09-27 16:01:22 +0900

    SB 0927 kakaologin commit.

--- Br_Project/src/main/java/com/br/controller/HomeController.java
+++ Br_Project/src/main/java/com/br/controller/HomeController.java
@@ -9,11 +9,14 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.br.service.NaverLoginBO;
 import com.github.scribejava.core.model.OAuth2AccessToken;
@@ -95,6 +98,9 @@ public class HomeController {
     public String error() {
     	return "etc/error";
     }
+    
+    	
+    
 }
 	
 
